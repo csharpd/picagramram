2a678cd1b3ba729248a1bf88a9b5ab7a0defeffe
csharpd <chloedonegan@gmail.com>
2014-08-28 17:37:09 +0100

    Added modals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :setup_new_post
+
+  def setup_new_post
+  @new_post = Post.new
+  end
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
